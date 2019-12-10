5d584a1459ba47122d950e538f2161e750752dd4
tmfrl9211 <wlsdyd9211@daum.net>
2019-12-10 11:38:19 +0900

    1210_main가져오기

--- hospital/src/main/java/kr/co/hospital/MainController.java
+++ hospital/src/main/java/kr/co/hospital/MainController.java
@@ -1,11 +1,7 @@
 package kr.co.hospital;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
@@ -13,5 +9,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 
 public class MainController {
+	
+	@RequestMapping(value="/main/main")
+	public String main()
+	{
+		return "/main/main";
+	}
+	
+	@RequestMapping(value="/main/index")
+	public String index()
+	{
+		return "/main/index";
+	}
+	
+	@RequestMapping(value="/main/sorry")
+	public String sorry()
+	{
+		return "/main/sorry";
+	}
 
 }
